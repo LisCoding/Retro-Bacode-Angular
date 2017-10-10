dca7459b62ca98df0726b6c5c0bedeed7ea33729
Liseth Cardozo <cardozoliseth@gmail.com>
2017-10-10 16:01:15 -0700

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  colors = ['red', 'blue', 'pink', 'black', 'orange', 'gray', 'green', 'yellow', 'purple' ];
+  random_colors = this.shuffleArray(this.colors);
+   shuffleArray(colors) {
+    for (var i = colors.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        [colors[i], colors[j]] = [colors[j], colors[i]];
+    }
+    return colors;
+  }
+
 }
